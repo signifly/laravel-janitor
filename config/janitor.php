706b5c12c2424180741872dbdfea5213546243a8
Morten Poul Jensen <mpj@signifly.com>
2019-03-05 13:41:52 +0100

    Add config

---
+++ config/janitor.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Janitor Driver
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default janitor driver that should be used
+    | by the framework.
+    |
+    */
+
+    'default' => env('JANITOR_DRIVER', ''),
+
+    /*
+     * The username field to user for authentication.
+     */
+    'username_field' => 'email',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Janitor Drivers
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure what is provided to the Janitor drivers.
+    | It must be an array.
+    |
+    | Supported Drivers: "jwt", "passport"
+    |
+    */
+
+    'drivers' => [
+
+        'jwt' => [],
+
+        'passport' => [
+            'oauth_token_url' => env('JANITOR_OAUTH_TOKEN_URL', 'http://localhost/oauth/token'),
+        ],
+
+    ],
+
+];
