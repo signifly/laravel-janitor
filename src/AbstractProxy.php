20c36fa80835245915c7007282cbfcb0c6b289b7
Morten Poul Jensen <mpj@signifly.com>
2019-03-06 13:49:38 +0100

    Add setter for username field

--- src/AbstractProxy.php
+++ src/AbstractProxy.php
@@ -7,11 +7,21 @@ use Signifly\Janitor\Contracts\Proxy;
 
 abstract class AbstractProxy implements Proxy
 {
+    /** @var array */
     protected $config;
 
+    /** @var string */
+    protected $usernameField;
+
+    /**
+     * Create a new instance.
+     *
+     * @param array $config
+     */
     public function __construct(array $config)
     {
         $this->config = $config;
+        $this->setUsernameField($config['username_field']);
     }
 
     /**
@@ -27,7 +37,7 @@ abstract class AbstractProxy implements Proxy
     /**
      * Get the user instance.
      *
-     * @return object
+     * @return \Illuminate\Contracts\Auth\Authenticatable
      */
     protected function getUserInstance()
     {
@@ -41,8 +51,21 @@ abstract class AbstractProxy implements Proxy
      *
      * @return string
      */
-    protected function getUsernameField()
+    public function getUsernameField(): string
     {
-        return 'email';
+        return $this->usernameField;
+    }
+
+    /**
+     * Set the username field.
+     *
+     * @param string $username
+     * @return $this
+     */
+    public function setUsernameField(string $username): self
+    {
+        $this->usernameField = $username;
+
+        return $this;
     }
 }
