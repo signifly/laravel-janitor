072ff86a744a69c98e550d848dd90dfd0cd52952
Morten Poul Jensen <mpj@signifly.com>
2019-03-06 13:33:49 +0100

    Extend AuthenticationException

--- src/Exceptions/InvalidCredentialsException.php
+++ src/Exceptions/InvalidCredentialsException.php
@@ -2,16 +2,16 @@
 
 namespace Signifly\Janitor\Exceptions;
 
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Auth\AuthenticationException;
 
-class InvalidCredentialsException extends HttpException
+class InvalidCredentialsException extends AuthenticationException
 {
     /** @var string */
     private $username;
 
     public static function forUsername(string $username): self
     {
-        $exception = new static(422, trans('auth.failed'));
+        $exception = new static(trans('auth.failed'));
         $exception->username = $username;
 
         return $exception;
@@ -19,7 +19,7 @@ class InvalidCredentialsException extends HttpException
 
     public static function withDefaultMessage(): self
     {
-        return new static(422, trans('auth.failed'));
+        return new static(trans('auth.failed'));
     }
 
     protected function getUsername(): string
